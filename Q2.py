b0fbfc142f21f1e79c3ac504139aba5fa58eadf7
liquan2003 <119789352+liquan2003@users.noreply.github.com>
2022-12-04 20:30:14 +0800

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,1 +1,19 @@
 #Question 2
+
+import math
+grd = 0.0
+loc = 0.0
+si = 0.0 
+
+for i in range(55):
+    grd = float(input())
+    loc = loc + grd / 55
+print(loc)
+for i in range(55):
+    grd = float(input())
+    si = si + (grd - loc)*(grd - loc) / 55
+si = math.sqrt(si)
+print(si)
+l = loc - 2 * si 
+r = loc + 2 * si
+print("(",l,",",r,")")
